768bc56df36e844e2fa5bdacfc5e2f0b7a69ee5e
laras126 <laras126@gmail.com>
2014-11-12 18:43:40 -0500

    add gear custom type

--- lib/custom-types.php
+++ lib/custom-types.php
@@ -59,104 +59,58 @@ function mtn_meister_post_type() {
 add_action( 'init', 'mtn_meister_post_type', 0 );
 
 
+
 /**
  *
- * Taxonomy for Meister Types
+ * Custom type for Gear
  *
  */
 
-// Register Custom Taxonomy
-function mtn_meister_industry_taxonomy() {
+// Register Custom Post Type
+function mtn_gear_post_type() {
 
 	$labels = array(
-		'name'                       => _x( 'Industry', 'Taxonomy General Name', 'mtnmeister' ),
-		'singular_name'              => _x( 'Industry', 'Taxonomy Singular Name', 'mtnmeister' ),
-		'menu_name'                  => __( 'Industries', 'mtnmeister' ),
-		'all_items'                  => __( 'All Industries', 'mtnmeister' ),
-		'parent_item'                => __( 'Parent Industry', 'mtnmeister' ),
-		'parent_item_colon'          => __( 'Parent Industry:', 'mtnmeister' ),
-		'new_item_name'              => __( 'New Industry', 'mtnmeister' ),
-		'add_new_item'               => __( 'Add Industry', 'mtnmeister' ),
-		'edit_item'                  => __( 'Edit Industry', 'mtnmeister' ),
-		'update_item'                => __( 'Update Industry', 'mtnmeister' ),
-		'separate_items_with_commas' => __( 'Separate items with commas', 'mtnmeister' ),
-		'search_items'               => __( 'Search Industries', 'mtnmeister' ),
-		'add_or_remove_items'        => __( 'Add or remove Industries', 'mtnmeister' ),
-		'choose_from_most_used'      => __( 'Choose from the most used Industries', 'mtnmeister' ),
-		'not_found'                  => __( 'Not Found', 'mtnmeister' ),
-	);
-	$rewrite = array(
-		'slug'                       => 'industry',
-		'with_front'                 => true,
-		'hierarchical'               => false,
+		'name'                => _x( 'Gear', 'Post Type General Name', 'mtnmeister' ),
+		'singular_name'       => _x( 'Gear', 'Post Type Singular Name', 'mtnmeister' ),
+		'menu_name'           => __( 'Gear', 'mtnmeister' ),
+		'parent_item_colon'   => __( 'Parent Gear:', 'mtnmeister' ),
+		'all_items'           => __( 'All Gear', 'mtnmeister' ),
+		'view_item'           => __( 'View Gear', 'mtnmeister' ),
+		'add_new_item'        => __( 'Add New Gear', 'mtnmeister' ),
+		'add_new'             => __( 'Add Gear', 'mtnmeister' ),
+		'edit_item'           => __( 'Edit Gear', 'mtnmeister' ),
+		'update_item'         => __( 'Update Gear', 'mtnmeister' ),
+		'search_items'        => __( 'Search Gear', 'mtnmeister' ),
+		'not_found'           => __( 'Not found', 'mtnmeister' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'mtnmeister' ),
 	);
 	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => true,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => true,
-		'rewrite'                    => $rewrite,
+		'label'               => __( 'gear', 'mtnmeister' ),
+		'description'         => __( 'Gear from episodes and deals.', 'mtnmeister' ),
+		'labels'              => $labels,
+		'supports'            => array( ),
+		'taxonomies'          => array( 'category', 'post_tag' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-shield',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
 	);
-	// register_taxonomy( 'meister_industry', array( 'meister' ), $args );
+	register_post_type( 'gear', $args );
 
 }
 
 // Hook into the 'init' action
-// add_action( 'init', 'mtn_meister_industry_taxonomy', 0 );
+add_action( 'init', 'mtn_gear_post_type', 0 );
 
-
-
-/**
- *
- * Taxonomy for Meister Tags
- *
- */
-
-// Register Custom Taxonomy
-function mtn_meister_tag_taxonomy() {
-
-	$labels = array(
-		'name'                       => _x( 'Meister Tags', 'Taxonomy General Name', 'mtnmeister' ),
-		'singular_name'              => _x( 'Meister Tag', 'Taxonomy Singular Name', 'mtnmeister' ),
-		'menu_name'                  => __( 'Meister Tags', 'mtnmeister' ),
-		'all_items'                  => __( 'All Meister Tags', 'mtnmeister' ),
-		'parent_item'                => __( 'Parent Meister Tag', 'mtnmeister' ),
-		'parent_item_colon'          => __( 'Parent Meister Tag:', 'mtnmeister' ),
-		'new_item_name'              => __( 'New Tag', 'mtnmeister' ),
-		'add_new_item'               => __( 'Add Tag', 'mtnmeister' ),
-		'edit_item'                  => __( 'Edit Tag', 'mtnmeister' ),
-		'update_item'                => __( 'Update Tag', 'mtnmeister' ),
-		'separate_items_with_commas' => __( 'Separate items with commas', 'mtnmeister' ),
-		'search_items'               => __( 'Search Meister Tags', 'mtnmeister' ),
-		'add_or_remove_items'        => __( 'Add or remove tags', 'mtnmeister' ),
-		'choose_from_most_used'      => __( 'Choose from the most used tags', 'mtnmeister' ),
-		'not_found'                  => __( 'Not Found', 'mtnmeister' ),
-	);
-	$rewrite = array(
-		'slug'                       => 'tag',
-		'with_front'                 => true,
-		'hierarchical'               => false,
-	);
-	$args = array(
-		'labels'                     => $labels,
-		'menu_icon'           		 => 'dashicons-groups',
-		'hierarchical'               => false,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => true,
-		'rewrite'                    => $rewrite,
-	);
-	// register_taxonomy( 'meister_tag', array( 'meister' ), $args );
-
-}
-
-// Hook into the 'init' action
-// add_action( 'init', 'mtn_meister_tag_taxonomy', 0 );
 
 
 /**
--- lib/utils.php
+++ lib/utils.php
@@ -2,9 +2,11 @@
 
 /**
  *
- * Change the title placeholder text for Custom Types
+ * Utility functions for cleaning up
  *
  */
+
+
 
 // Meister title placeholder
 function mtn_custom_type_title_text ( $title ) {
@@ -12,10 +14,34 @@ function mtn_custom_type_title_text ( $title ) {
 		$title = __( 'Meister Name' );
 	} else if ( get_post_type() == 'deal' ) {
         $title = __( 'Deal Title' );
+	} else if ( get_post_type() == 'gear' ) {
+        $title = __( 'Gear Name' );
 	}
 	return $title;
 } // End title_text_input()
 
 add_filter( 'enter_title_here', 'mtn_custom_type_title_text' );
+
+
+
+
+// Remove unused items from the Dashboard menu
+function mtn_remove_menu_items(){
+	remove_menu_page( 'edit.php' ); // Posts
+	remove_menu_page( 'edit-comments.php' ); // Posts
+	remove_menu_page( 'users.php' ); // Users
+}
+add_action( 'admin_menu', 'mtn_remove_menu_items' );
+
+
+// Remove new post/user/media from +New menu item in the admin bar
+function mtn_remove_wp_nodes() {
+    global $wp_admin_bar;   
+    $wp_admin_bar->remove_node( 'new-post' );
+    $wp_admin_bar->remove_node( 'new-link' );
+    $wp_admin_bar->remove_node( 'new-media' );
+    $wp_admin_bar->remove_node( 'new-user' );
+}
+add_action( 'admin_bar_menu', 'mtn_remove_wp_nodes', 999 );
 
 ?>

c228bc77b9b30b01f43df0c193c64fde8c20d823
laras126 <laras126@gmail.com>
2014-11-13 15:01:26 -0500

    more twitter btn styling

--- functions.php
+++ functions.php
@@ -67,4 +67,14 @@
 	require_once('lib/utils.php'); 
 
 
+function namespace_add_custom_types( $query ) {
+  if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+    $query->set( 'post_type', array(
+     'post', 'nav_menu_item', 'gear'
+		));
+	  return $query;
+	}
+}
+add_filter( 'pre_get_posts', 'namespace_add_custom_types' );
+
 ?>

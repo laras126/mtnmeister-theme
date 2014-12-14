55eff530554cbabb97563da83f03170fde1a4210
laras126 <laras126@gmail.com>
2014-12-14 11:46:45 -0500

    lazyload placeholder

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
 	// Plugins
 	$('.page-title').fitText(0.65, {minFontSize: '45px'});
 	$('.lazy').lazyload({
-		effect : 'fadeIn'
+		effect : 'fadeIn',
+		threshold : 600
 	}); // Am I lazy for using this?
 
 

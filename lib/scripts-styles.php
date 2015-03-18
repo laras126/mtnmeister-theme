26d567b5ec6d14e447e7374a4f8f02a64ac2ca5b
laras126 <laras126@gmail.com>
2015-03-18 13:28:28 -0400

    refactor block dirs, add conditional for text header

--- lib/scripts-styles.php
+++ lib/scripts-styles.php
@@ -1,33 +1,35 @@
 <?php 
 
 
-// Enqueue styles
-function mtn_styles() {
-  wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/css/main.min.css' );
+// Enqueue scripts and styles
+function mtn_scripts_styles() {
+
+    // Use jQuery from CDN
+    if (!is_admin()) {
+        wp_deregister_script('jquery');
+        wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js');
+        wp_enqueue_script('jquery', array(), null, true);
+    }
+
+    if( WP_ENV == 'production' ) {
+        wp_enqueue_style( 'nl-styles', get_template_directory_uri() . '/assets/css/main.min.css', 1.0);
+        wp_register_script('mtn-js', get_template_directory_uri() . '/assets/js/build/scripts.min.js');
+    } else {
+        wp_enqueue_style( 'nl-styles', get_template_directory_uri() . '/assets/css/main.css', 1.0);
+        wp_register_script('mtn-js', get_template_directory_uri() . '/assets/js/scripts.js');
+
+    }
+
+    // Add MTNmeister JS
+    // wp_register_script('mtn-js', get_template_directory_uri() . '/assets/js/scripts.js');
+    
+    wp_enqueue_script( 'mtn-js', array('jquery'), null, true );
+
+    if (is_single() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
+    }
 }
-add_action( 'wp_enqueue_scripts', 'mtn_styles' );
-
-
-// Enqueue scripts
-function mtn_scripts() {
-
-  // Use jQuery from CDN
-  if (!is_admin()) {
-    wp_deregister_script('jquery');
-    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js');
-    wp_enqueue_script('jquery', array(), null, true);
-  }
-
-  // Add MTNmeister JS
-  // wp_register_script('mtn-js', get_template_directory_uri() . '/assets/js/scripts.js');
-  wp_register_script('mtn-js', get_template_directory_uri() . '/assets/js/build/scripts.min.js');
-  wp_enqueue_script( 'mtn-js', array('jquery'), null, true );
-
-  if (is_single() && comments_open() && get_option('thread_comments')) {
-    wp_enqueue_script('comment-reply');
-  }
-}
-add_action( 'wp_footer', 'mtn_scripts' );
+add_action( 'wp_enqueue_scripts', 'mtn_scripts_styles' );
 
 
 

1872a583aecfe129810a114ad759ef6988777b67
laras126 <laras126@gmail.com>
2015-03-18 12:15:13 -0400

    add yoast custom fields function

--- lib/utils.php
+++ lib/utils.php
@@ -23,6 +23,7 @@ function mtn_custom_type_title_text ( $title ) {
 add_filter( 'enter_title_here', 'mtn_custom_type_title_text' );
 
 
+
 // Remove unused items from the Dashboard menu
 function mtn_remove_menu_items(){
 	remove_menu_page( 'edit.php' ); // Posts
@@ -30,6 +31,7 @@ function mtn_remove_menu_items(){
 	remove_menu_page( 'users.php' ); // Users
 }
 add_action( 'admin_menu', 'mtn_remove_menu_items' );
+
 
 
 // Remove new post/user/media from +New menu item in the admin bar
@@ -41,6 +43,7 @@ function mtn_remove_wp_nodes() {
     $wp_admin_bar->remove_node( 'new-user' );
 }
 add_action( 'admin_bar_menu', 'mtn_remove_wp_nodes', 999 );
+
 
 
 // Add a 'Very Simple' toolbar style for the WYSIWYG editor in ACF
@@ -57,12 +60,41 @@ function mtn_acf_wysiwyg_toolbar( $toolbars ) {
 add_filter( 'acf/fields/wysiwyg/toolbars' , 'mtn_acf_wysiwyg_toolbar'  );
 
 
+
 // Customize the editor style, from Roots.io 
 // https://github.com/roots/roots-sass/blob/master/assets/css/editor-style.css
 function mtn_editor_styles() {
 	add_editor_style( 'assets/css/editor-style.css' );
 }
 add_action( 'after_setup_theme', 'mtn_editor_styles' );
+
+
+// Make custom fields work with Yoast SEO (only impacts the light, but helpful!)
+// https://imperativeideas.com/making-custom-fields-work-yoast-wordpress-seo/
+
+if ( is_admin() ) { // check to make sure we aren't on the front end
+	add_filter('wpseo_pre_analysis_post_content', 'mtn_add_custom_to_yoast');
+
+	function mtn_add_custom_to_yoast( $content ) {
+		global $post;
+		$pid = $post->ID;
+		
+		$custom = get_post_custom($pid);
+		unset($custom['_yoast_wpseo_focuskw']); // Don't count the keyword in the Yoast field!
+
+		foreach( $custom as $key => $value ) {
+			if( substr( $key, 0, 1 ) != '_' && substr( $value[0], -1) != '}' && !is_array($value[0]) && !empty($value[0])) {
+			  $custom_content .= $value[0] . ' ';
+			}
+		}
+
+		$content = $content . ' ' . $custom_content;
+		return $content;
+
+		remove_filter('wpseo_pre_analysis_post_content', 'mtn_add_custom_to_yoast'); // don't let WP execute this twice
+	}
+}
+
 
 
 ?>

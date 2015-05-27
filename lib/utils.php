ad1dae83730483990d583d1bf90862e551fb176c
laras126 <laras126@gmail.com>
2015-05-27 11:59:44 -0400

    add picture partials and clean up files

--- lib/utils.php
+++ lib/utils.php
@@ -24,20 +24,10 @@ add_filter( 'enter_title_here', 'mtn_custom_type_title_text' );
 
 
 
-// Remove unused items from the Dashboard menu
-function mtn_remove_menu_items(){
-	// remove_menu_page( 'edit.php' ); // Posts
-	// remove_menu_page( 'edit-comments.php' ); // Posts
-	remove_menu_page( 'users.php' ); // Users
-}
-add_action( 'admin_menu', 'mtn_remove_menu_items' );
 
-
-
-// Remove new post/user/media from +New menu item in the admin bar
+// Remove new user/media from +New menu item in the admin bar
 function mtn_remove_wp_nodes() {
     global $wp_admin_bar;   
-    $wp_admin_bar->remove_node( 'new-post' );
     $wp_admin_bar->remove_node( 'new-link' );
     $wp_admin_bar->remove_node( 'new-media' );
     $wp_admin_bar->remove_node( 'new-user' );

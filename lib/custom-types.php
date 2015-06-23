8e94079f3417097b54365a20a486046fed4fddfb
laras126 <laras126@gmail.com>
2015-06-23 11:36:53 -0400

    add custom type

--- lib/custom-types.php
+++ lib/custom-types.php
@@ -148,6 +148,48 @@ $args = array(
 );
 register_post_type( 'deal', $args );
 
+/**
+ *
+ * Custom type for Meister Deals
+ *
+ */
 
+$labels = array(
+		'name'                => _x( 'Header Images', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Header Image', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Header Images', 'text_domain' ),
+		'name_admin_bar'      => __( 'Header Image', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
+		'all_items'           => __( 'All Images', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Image', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'new_item'            => __( 'New Image', 'text_domain' ),
+		'edit_item'           => __( 'Edit Image', 'text_domain' ),
+		'update_item'         => __( 'Update Image', 'text_domain' ),
+		'view_item'           => __( 'View Image', 'text_domain' ),
+		'search_items'        => __( 'Search Image', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$args = array(
+		'label'               => __( 'header_image', 'text_domain' ),
+		'description'         => __( 'Large Header Images', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( ),
+		'taxonomies'          => array( 'category', 'post_tag' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'menu_position'       => 5,
+		'show_in_admin_bar'   => true,
+		'show_in_nav_menus'   => true,
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => true,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'header_image', $args );
 
 ?>
